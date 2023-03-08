be7646b2a602b516dd3371bf0e43c15252ce0b2a
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-03-09 01:01:23 +0530

    Update SubjectsDetailsImpl.java

--- SaleskenA/src/main/java/com/salesken/service/SubjectsDetailsImpl.java
+++ SaleskenA/src/main/java/com/salesken/service/SubjectsDetailsImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.salesken.exception.SubjectDetailsException;
 import com.salesken.repository.SubjectDetailsRepo;
 
-@service;
+@service
 public class SubjectsDetailsImpl implements SubjectsDetailsInterface {
 
 	@Autowired
